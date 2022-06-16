0b43bd6d9ec1c581e89d0e62d80823c102a7ec4c
milind-16 <milind.gole16@gmail.com>
2022-06-16 17:21:44 +0530

    initial

---
+++ src/main/java/com/water/app/RainwaterApplication.java
@@ -0,0 +1,13 @@
+package com.water.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainwaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainwaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/water/app/RainwaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.water.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainwaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

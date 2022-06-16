c7f06d7ac897b2d9e1479b712bd06dc96b97381e
jett-lag-7 <107622745+jett-lag-7@users.noreply.github.com>
2022-06-16 18:06:23 +0530

    jett comm 2

--- src/main/java/com/water/app/controller/TradeController.java
+++ src/main/java/com/water/app/controller/TradeController.java
@@ -13,7 +13,7 @@ import com.water.app.model.Response;
 public class TradeController {
 	
 	/**
-	 * Fetches all active accounts jett1
+	 * Fetches all active accounts jett2
 	 * @return
 	 */
 	@GetMapping(value = "/get_all_accounts")
